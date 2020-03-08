a0de749fe7fab29443dc9ea45a0aa6e6ddcbe247
thenewcq <60067794+thenewcq@users.noreply.github.com>
2020-03-08 11:29:10 -0400

    Add files via upload

---
+++ get_path.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+        char cwd[PATH_MAX];
+
+        if(getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+                printf("Current working dir: %s\n", cwd);
+        }
+        else
+        {
+                perror("getcwd() error");
+                return 1;
+        }
+
+   return 0;
+}
